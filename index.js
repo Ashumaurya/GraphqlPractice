88593a949d358c6ec661b6a9963ec15e7341b5b6
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-06-07 01:29:21 +0530

    Schema file added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(
+  "/graphqlHTTP",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
 app.listen(port, () => console.log(`Example app listening on port port!`));
