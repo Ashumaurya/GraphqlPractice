5a11d0a90d3e9bd7f53f3bfc3b81a92ab0c5cb44
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-06-08 01:48:54 +0530

    GraphQL working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 const port = 3000;
 
@@ -7,6 +8,11 @@ app.use(
   "/graphqlHTTP",
   graphqlHTTP({
     graphiql: true,
+    schema,
   })
 );
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () =>
+  console.log(
+    `Example app listening on port port! Running on http://localhost:3000/graphqlHTTP`
+  )
+);
--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,19 @@
 const graphql = require("graphql");
-
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
-
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+//Hard Coded DATA
+const users = [
+  {
+    id: "23",
+    firstName: "Bill",
+    age: 20,
+  },
+  {
+    id: "47",
+    firstName: "Samantha",
+    age: 21,
+  },
+];
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
@@ -9,4 +21,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
