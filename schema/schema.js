a1f9400a5eda82db1fbcac2e085197412926e4b0
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-06-11 22:21:39 +0530

    CRUD working project Finished

--- schema/schema.js
+++ schema/schema.js
@@ -94,6 +94,34 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    deleteUser: {
+      type: UserType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) } },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data);
+      },
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, { id, firstName, age, companyId }) {
+        console.log(parentValue);
+        return axios
+          .patch(`http://localhost:3000/users/${id}`, {
+            firstName,
+            age,
+            companyId,
+          })
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
